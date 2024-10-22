459810c2da7f9dde5c5f348c4ed49e500e4445bc
sifanac1999 <sifanac1999@gmail.com>
2024-10-22 17:15:01 +0300

    first comment

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const buttons = document.querySelectorAll('.about-btn button');
+const contents = document.querySelectorAll('.content');
+
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    contents.forEach(content => content.style.display = 'none');
+    contents[index].style.display = 'block';
+    buttons.forEach(btn => btn.classList.remove('active'));
+    button.classList.add('active');
+  });
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxmS0Iel5FRsVMMLDFpYLeXlXFH4HBDDaVtOJwsF5f6_lCjOxaSU9FwfJH8wMghdBzDOQ/exec'
+    const form = document.forms['submit-to-google-sheet']
+  
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
