b329da41dfee039580c7d5e0f7a2ffef14804f2f
sifanac1999 <sifanac1999@gmail.com>
2024-10-22 22:20:47 +0300

    first comment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+let menuIcon = document.querySelector(".menu-icon");
+let navlist = document.querySelector(".navlist")
+menuIcon.addEventListener("click",()=>{
+    menuIcon.classList.toggle("active");
+    navlist.classList.toggle("active");
+    document.body.classList.toggle("open");
+});
+
+
+navlist.addEventListener("click",()=>{
+    navlist.classList.remove("active");
+    menuIcon.classList.remove("active");
+    document.body.classList.remove("open");
+})
+
 const buttons = document.querySelectorAll('.about-btn button');
 const contents = document.querySelectorAll('.content');
 
@@ -19,3 +34,8 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbxmS0Iel5FRsVMMLDFpYL
         .then(response => console.log('Success!', response))
         .catch(error => console.error('Error!', error.message))
     })
+
+    ScrollReveal().reveal('.sc-info,.heading', { origin: "top" });
+    ScrollReveal().reveal('.about-img,.contact-info', { origin: "left" });
+    ScrollReveal().reveal('.about-content,.skills', { origin: "right" });
+    ScrollReveal().reveal('.project-gallery,.img-sc', { origin: "bottom" });
